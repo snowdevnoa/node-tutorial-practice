3052a0ed0fec0107bf079f12e358989ec1b5e214
Tam Duong <tduon31@wgu.edu>
2024-01-23 11:27:09 -0600

    request event using http

---
+++ app.js
@@ -0,0 +1,12 @@
+/* 
+Streams
+streams are used to read or write sequentially. when we have to handle/manipulate streaming data(continous source or a big file)
+
+4 different types of streams:
+Writeable - write data
+Readable - read data
+Duplex - read and write data
+Transform - data can be modified when writing or reading 
+
+streams EXTEND EventEmitter Class meaning we can use events like data and and on streams
+*/
